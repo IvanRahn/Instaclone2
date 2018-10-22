794719bfa7337642bd8636d70a95f71f0bab2510
Ivan Rahn <ivan.rahn@gmail.com>
2018-10-22 14:47:04 +1100

    add thumbnails

--- app/uploaders/main_image_uploader.rb
+++ app/uploaders/main_image_uploader.rb
@@ -1,7 +1,7 @@
 class MainImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :aws
@@ -22,10 +22,13 @@ class MainImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  # process resize_to_fill: [200, 200]
   #
-  # def scale(width, height)
-  #   # do something
+  version :thumb do
+process resize_to_fill: [200,200]
+  end
+  # def resize_to_fit(width, height)
+
   # end
 
   # Create different versions of your uploaded files:
